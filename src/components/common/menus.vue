48189f72d28700835b2b3c07bd1beeb62a1021bf
liuyinggang <952904268@qq.com>
2023-01-29 21:20:52 +0800

    lastupdate

--- src/components/common/menus.vue
+++ src/components/common/menus.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
-    <el-menu :defalut-active="activeIndex" class="el-menu" mode="horizontal" @select="handleSelect">
-
-
-        <el-menu-item v-for="item in array " :key="item">
-            <a href="JavaScript:void(0);">{{item}}</a>
-        </el-menu-item>
-
-    </el-menu>
+    <div id="header">
+        <div class="menus">
+            <div class="logo"><img src="@/assets/logo.png"></div>
+            <div class="navs">
+                <ul class="nav-bar">
+                    <li class="nav-bar-items" v-for="items in names" :key="items"><a href="javascript:void(0);">{{items}}</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
   </div>
 </template>
 
@@ -15,22 +17,59 @@
 export default {
     data() {
         return {
-            activeIndex: '1',
-            array: ['首页', '项目内容', '合作加盟', '招才纳士']
+            names: ['首页', '我的动态', '关于我们', '共创合作', '招才纳士']
         }
     }
 }
 </script>
 
 <style>
-
-.el-menu{
-    border-bottom: none !important;  
-    float: right;
-    
+#header{
+    /* background-color: blue; */
+    width: 100%;
+    height: 100px;
+    /* margin: 0px 100px; */
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 10;
+    border-bottom: 1px solid #ccc;
 }
-.el-menu-item a{
+.logo, .navs{
+    float: left;
+    position: relative;
+    top: 20px;
+    text-align: center;
+}
+.logo{
+    width: 134px;
+    height: 70px;
+    line-height: 70px;
+    background-color: aqua;
+    margin-left: 200px;
+}
+.navs{
+    width: 900px;
+    height: 70px;
+    background-color: aqua;
+}
+.navs .nav-bar{
+    float: right;
+    list-style: none;
+    background-color: aquamarine;
+    line-height: 70px;
+    position: relative;
+    top: -16px;
+}
+.navs .nav-bar .nav-bar-items{
+    /* color: #fff; */
+    float: left;
+    width: 80px;
+    margin: 0px 25px;
+    background-color: blanchedalmond;
+}
+.navs .nav-bar .nav-bar-items a{
+    color: #ccc;
     text-decoration: none;
-    color: #000 !important;
 }
 </style>
